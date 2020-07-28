7c5b18df83d4bb01bca15529d87d46675de67524
xiaokaikiane <1131697410@qq.com>
2020-07-28 21:54:00 +0800

    多线程

---
+++ src/test.java
@@ -0,0 +1,24 @@
+public class test {
+    private static String commStr;
+    private static ThreadLocal<String> threadStr = new ThreadLocal<String>();
+    public static void main(String[] args) {
+        commStr = "main";
+        threadStr.set("main");
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                commStr = "thread";
+                threadStr.set("thread");
+                System.out.println(threadStr.get());
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(commStr);
+        System.out.println(threadStr.get());
+    }
+}
