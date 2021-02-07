ca4b59aafa07f37c4eefe4b016f6022a25835347
xiaokaikiane <1131697410@qq.com>
2021-02-07 12:24:55 +0800

    多线程

--- src/CallableRun.java
+++ src/CallableRun.java
@@ -19,15 +19,23 @@ public class CallableRun {
     }
     public static  void test2()throws ExecutionException,InterruptedException{
         ExecutorService pool= Executors.newFixedThreadPool(4);
-        Future future=pool.submit(new Callable<String>() {
+//        Future future=pool.submit(new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                System.out.println(Thread.currentThread().getName()+":call");
+//                return "call finish";
+//            }
+//        });
+        FutureTask<?> futureTask=new FutureTask<>(new Callable<Object>() {
             @Override
             public String call() throws Exception {
                 System.out.println(Thread.currentThread().getName()+":call");
                 return "call finish";
             }
         });
+        pool.submit(futureTask);
         System.out.println(Thread.currentThread().getName()+":pool submit ,before get()");
-        System.out.println(Thread.currentThread().getName()+" get:"+ future.get());
+        System.out.println(Thread.currentThread().getName()+" get:"+ futureTask.get());
     }
     public static void main(String[] args)throws Exception {
         test1();
